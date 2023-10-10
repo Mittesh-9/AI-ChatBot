cf0c2ed3d1e9fe271720e9bb9d34dd0da2609063
Mittesh <mitteshwaghule09@gmail.com>
2023-10-10 18:27:32 +0530

    modified main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import openai
 
+openai.api_key = "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"
+
+def chat_with_gpt(prompt):
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = [{"role" : "user", "content" : prompt}]
+    )
+
+    return response.choices[0].message.content.strip()
+
+if __name__ =="__main__":
+    while True :
+        user_input = input("You:")
